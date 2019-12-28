b2b03e2c993118f7556bca605658b4b0eb235e85
SebastianLie <jackcass629@gmail.com>
2019-12-28 17:34:54 +0800

    Add proper working carousel to home page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    items:1,
+    nav: true,
+   
     loop:true,
     margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-  })
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
