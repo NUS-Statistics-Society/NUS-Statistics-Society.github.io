fcde06798b1639bd9cfec2ff7e75b505a4a0ca80
SebastianLie <jackcass629@gmail.com>
2019-12-28 21:19:20 +0800

    Add arrows to navigate splashscreen of home page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ owl.owlCarousel({
     center: true,
     items:1,
     nav: true,
-   
+    navText: ['<i class="icon-arrow-left" aria-hidden="true"></i>','<i class="icon-arrow-right" aria-hidden="true"></i>'],
     loop:true,
     margin:10,
     autoplay:true,
